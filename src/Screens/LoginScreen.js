2e58425bb5e43c39d7c00752eb7d2ed883006a66
rabiulhasan3 <rabiul.fci@gmail.com>
2022-01-01 12:58:34 +0600

    login screen desing

--- src/Screens/LoginScreen.js
+++ src/Screens/LoginScreen.js
@@ -1,12 +1,56 @@
 import React from 'react';
-import {Text,View} from 'react-native';
+import {
+    Button,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View
+} from 'react-native';
 
 const LoginScreen = () => {
-    return (
-        <View>
-            <Text>LoginScreen</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.wrapper}>
+        <TextInput style={styles.input} placeholder="Enter email" />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter password"
+          secureTextEntry
+        />
+        <Button title="Login" />
+
+        <View style={{flexDirection: 'row', marginTop: 20}}>
+          <Text>Don't have an account ? </Text>
+          <TouchableOpacity>
+            <Text style={styles.link}>Register</Text>
+          </TouchableOpacity>
         </View>
-    )
-}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  wrapper: {
+    width: '80%',
+  },
+  input: {
+    marginBottom: 12,
+    borderWidth: 1,
+    borderColor: '#bbb',
+    borderRadius: 2,
+    paddingHorizontal: 15,
+    paddingVertical: 8,
+  },
+  link: {
+    color: 'blue',
+  },
+});
 
 export default LoginScreen;
