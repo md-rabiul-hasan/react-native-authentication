3a0b714dac392c2a841c64503d1883ac19878d0e
rabiulhasan3 <rabiul.fci@gmail.com>
2022-01-01 12:35:44 +0600

    screen and naviation page create

---
+++ src/Component/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+
+const NavigationScreen = () => {
+    return (
+        <View>
+            <Text>NavigationScreen</Text>
+        </View>
+    )
+}
+
+export default NavigationScreen;
