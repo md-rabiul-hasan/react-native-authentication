3a66129e925418eb0ad0e35d83ba6b32cbc59d9d
rabiulhasan3 <rabiul.fci@gmail.com>
2022-01-01 12:44:12 +0600

    navigation setup

--- src/Component/Navigation.js
+++ src/Component/Navigation.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import {Text,View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../Screens/LoginScreen';
+import RegisterScreen from '../Screens/RegisterScreen';
+import HomeScreen from '../Screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 const NavigationScreen = () => {
     return (
-        <View>
-            <Text>NavigationScreen</Text>
-        </View>
-    )
-}
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Register" component={RegisterScreen} />
+                <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
 
 export default NavigationScreen;
